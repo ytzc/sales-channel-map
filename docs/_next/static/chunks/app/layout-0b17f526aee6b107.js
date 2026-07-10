0b4dce7a59cf0dcb644c63d3453ca8790dd5b391
jerry.yang <ytz1298@gmail.com>
2026-07-10 14:52:21 +0800

    feat: deploy static PRD page and compiled Next.js MVP demo to GitHub Pages

---
+++ docs/_next/static/chunks/app/layout-0b17f526aee6b107.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{8233:function(n,e,t){Promise.resolve().then(t.t.bind(t,2972,23)),Promise.resolve().then(t.t.bind(t,2778,23))},2778:function(){}},function(n){n.O(0,[461,972,971,117,744],function(){return n(n.s=8233)}),_N_E=n.O()}]);
